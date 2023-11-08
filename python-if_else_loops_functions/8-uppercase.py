f78ac95722a0de3f6c9cff0826e0e07570851b0d
gerberth92 <gerberthcontreras@gmail.com>
2023-11-07 19:39:54 -0800

    reduciendo de 3 a 2 print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,12 @@
 def uppercase(str):
 
     for i in range(len(str)):
+
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             c = 32
-            print("{:c}".format(ord(str[i]) - c), end='')
         else:
-            print("{:c}".format(ord(str[i])), end='')
+            c = 0
+
+        print("{:c}".format(ord(str[i]) - c), end='')
+
     print()
