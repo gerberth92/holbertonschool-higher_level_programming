a906f2f7b8c99c3c6f59e12cc5f67c3172a1a7e2
gerberth92 <gerberthcontreras@gmail.com>
2023-11-09 12:16:00 -0800

    ordenando el codigo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 
-from sys import argv
-
-
-def main()
-
-argumento = argv
-c = len(argv)
-
-if c == 1:
-    print(f"{c-1} arguments.")
-
-if c == 2:
-    print("1 argument:")
-
-if c > 2:
-    print(f"{c-1} arguments:")
-
-for i in range(1, c):
-    print(f"{i}: {argv[i]}")
-
 
 if __name__ == "__main__":
-    main()
+
+    from sys import argv
+
+    def main()
+
+    argumento = argv
+    c = len(argv)
+
+    if c == 1:
+        print(f"{c-1} arguments.")
+
+    if c == 2:
+        print("1 argument:")
+
+    if c > 2:
+        print(f"{c-1} arguments:")
+
+    for i in range(1, c):
+        print(f"{i}: {argv[i]}")
