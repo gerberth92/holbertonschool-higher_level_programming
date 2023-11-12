284b4f27cc8e14fe1fd99527952a3371f6562e60
gerberth92 <gerberthcontreras@gmail.com>
2023-11-12 09:14:48 -0800

    creando una nueva string

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    s = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            s = s + my_string[i]
+    return (s)
