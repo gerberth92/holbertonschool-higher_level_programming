ba9ebb42c34fea165a4bb867a85e92d969843ecc
gerberth92 <gerberthcontreras@gmail.com>
2023-11-11 20:03:25 -0800

    creando una copia de lista

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new = my_list.copy()
+    if idx < 0:
+        return (new)
+    elif idx > len(my_list) - 1:
+        return (new)
+    else:
+        new[idx] = element
+        return (new)
