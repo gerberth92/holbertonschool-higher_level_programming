c107a1d63b41630a34fabf8dbf8c050356e92121
gerberth92 <gerberthcontreras@gmail.com>
2023-11-13 13:41:43 -0800

    cambiando de nomre a la variable

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    l = []
+    lista = []
     for i in matrix:
-        l.append(list(map(lambda x: x * x, i)))
-    return (l)
+        lista.append(list(map(lambda x: x * x, i)))
+    return (lista)
