a74a1135c5226c20e4421df515a7087aedaa02ac
gerberth92 <gerberthcontreras@gmail.com>
2023-11-12 17:36:33 -0800

    update

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        new = (len(sentence), None)
+        new = (0, None)
     else:
         new = (len(sentence), sentence[0])
     return(new)
