8edb61c55de3a910b41193bcd7d430148d07d88b
gerberth92 <gerberthcontreras@gmail.com>
2023-11-14 07:55:54 -0800

    ordenando la lista

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for llave, valor in sorted(a_dictionary.items()):
-        print(llave, valor)
+    lista = sorted(list(a_dictionary))
+    for i in lista:
+        print("{}: {}".format(i, a_dictionary.get(i)))
