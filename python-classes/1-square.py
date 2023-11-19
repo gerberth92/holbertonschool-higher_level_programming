0f393fc9d424984c255fa366e78b3278eccfa327
gerberth92 <gerberthcontreras@gmail.com>
2023-11-19 08:17:02 -0800

    atributo privado

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Esta es una clase
+"""
+
+
+class Square:
+    """
+    esta es una clase de nombre square
+    """
+    def __init__(self, size):
+        """
+        inicializando un metodo que acepta un argumento size
+        """
+        self.__size = size
+        """
+        este es un atributo privado
+        """
