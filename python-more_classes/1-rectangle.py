7e6c1adbf38716632b2a65a57f75f54c7ec8a6b2
gerberth92 <gerberthcontreras@gmail.com>
2023-11-20 12:58:41 -0800

    agregando comentarios

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,26 +8,43 @@ class Rectangle:
     """
     Esta funcion define un rectangulo y tiene dos argumentos.
 
-    Args:
-        width (int): arguento 1 que es un entero mayor que 0.
-        height (int): argumento 2 que es un entero mayor que 0.
-
-    Raises:
-        TypeError: si width no es un entero.
-        TypeError: si height no es un entero.
-        ValueError: si width es menor que 0.
-        ValueError: si height es menor que 0.
+    Attributes:
+        __width (int): atributo privado.
+        __height (int): atributo privado.
     """
     def __init__(self, width=0, height=0):
+        """
+        Iniciando un metodo constructor.
+
+        Args:
+            width (int): es un entero mayor o igual que 0.
+            height (int): es un entero mayor o igual que 0.
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """
+        Metodo que captura width.
+
+        Returns:
+            Retorna el valor de width.
+        """
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """
+        Metodo que cambia el valor de width.
+
+        Args:
+            value (int): es de tipo entero mayor o igual a 0.
+
+        Raises:
+            TypeError: si width no es un entero.
+            ValueError: si width es menor que 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -36,10 +53,26 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        Metodo que captura height.
+
+        Returns:
+            Retorna el valor de height.
+        """
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """
+        Metodo que asigna el valor a height.
+
+        Args:
+            value (int): es de tipo entero mayor o igual que 0.
+
+        Raises:
+            TypeError: si height no es un entero.
+            ValueError: si height es menor que 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
