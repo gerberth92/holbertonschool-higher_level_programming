e23f6bd5323341d86f93723dd80357d8dce16ab5
gerberth92 <gerberthcontreras@gmail.com>
2023-11-20 13:35:09 -0800

    agregando validacion

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -95,4 +95,6 @@ class Rectangle:
         Return:
             Retorna el perimetro de un rectangulo.
         """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return ((2 * self.__width) + (2 * self.__height))
