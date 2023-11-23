e0cce09181fd5ec30b1572b9ad0a327a2a999ffe
gerberth92 <gerberthcontreras@gmail.com>
2023-11-23 12:11:51 -0800

    agregando documentacion

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-
+"""
+Este modulo importa una funcion y define una clase rectangle con herencia.
+"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """
+    Esta funcion hereda de BaseGeometry y tiene un constructor.
+    """
     def __init__(self, width, height):
+        """
+        Iniciando el constructor que acepta dos argumentos.
+
+        Args:
+            width (int): argumento de tipo int
+            height (int): argumento de tipo int
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
