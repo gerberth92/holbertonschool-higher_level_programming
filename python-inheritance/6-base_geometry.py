eedc255fd92daa87945e66c45a6cb9cc239ffbab
gerberth92 <gerberthcontreras@gmail.com>
2023-11-23 10:01:29 -0800

    funcion area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Este modulo tienen una clase que define una base geometrica.
+"""
+
+
+class BaseGeometry:
+    """
+    Esta clase define una base geometrica.
+    """
+    def area(self):
+        """
+        Esta funcion levanta una excepcion.
+
+        Raises:
+            Exception: el area no esta implementada.
+        """
+        raise Exception("area() is not implemented")
