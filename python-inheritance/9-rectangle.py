f81fb5ee9861c7cd0d494878fb6c717bc8b8ca8c
gerberth92 <gerberthcontreras@gmail.com>
2023-11-23 12:30:25 -0800

    calculando el area

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Este modulo importa una funcion y define una clase rectangle con herencia.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Esta funcion hereda de BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Iniciando el constructor que acepta dos argumentos.
+
+        Args:
+            width (int): argumento de tipo int
+            height (int): argumento de tipo int
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        """
+        Esta funcion retorna un mensaje.
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """
+        Esta funcion retorna el area del rectangulo.
+        """
+        return (self.__width * self.__height)
