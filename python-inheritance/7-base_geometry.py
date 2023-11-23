85db9b5c828991006195a9ced3f8652109f6a184
gerberth92 <gerberthcontreras@gmail.com>
2023-11-23 11:40:01 -0800

    cambiando por type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry:
             TypeError: si es diferente de (int).
             ValuError: si es menor o igual a 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
