d56caad55e93abd8cb930af4a89a5ac5d8f1172c
gerberth92 <gerberthcontreras@gmail.com>
2023-11-25 11:21:10 -0800

    agregando comentarios

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Este modulo tiene una funcion que lee.
+"""
+
 
 def read_file(filename=""):
+    """
+    Esta funcion abre, lee y cierra un archivo.
+
+    Args:
+        filename (txt): este es el archivo que se abrira.
+    """
     with open(filename, 'r', encoding="utf-8") as archivo:
         contenido = archivo.read()
     print(contenido)
