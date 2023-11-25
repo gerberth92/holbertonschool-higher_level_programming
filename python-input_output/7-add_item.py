85ab298d68a4415b609ee75588c394ce0ee388a1
gerberth92 <gerberthcontreras@gmail.com>
2023-11-25 15:21:05 -0800

    usando argv

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Este modulo captura los argumentos pasados.
+
+lista (list): lista donde se guardaran los argumentos pasados.
+"""
+
+from sys import argv
+import os.path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+lista = []
+if os.path.exists("add_item.json"):
+    lista = load_from_json_file("add_item.json")
+
+for i in argv[1:]:
+    lista.append(i)
+
+save_to_json_file(lista, "add_item.json")
