778be8a727206b55375a177807cf222de89f89f4
gerberth92 <gerberthcontreras@gmail.com>
2023-11-25 11:25:30 -0800

    agregando espacio

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -13,4 +13,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding="utf-8") as archivo:
         contenido = archivo.read()
-    print(contenido)
+        print(contenido, end='')
