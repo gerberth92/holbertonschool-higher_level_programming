a59dc7ccb051e56f7015edc451e3da1ec34c4fc1
gerberth92 <gerberthcontreras@gmail.com>
2023-11-25 16:03:49 -0800

    corrigiendo nombres

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -17,9 +17,9 @@ class Student:
             last_name: apellido
             age: edad
         """
-        self.nombre = first_name
-        self.apellido = last_name
-        self.edad = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """
