0c2f7a379348185b3ee0787d850e80836afd7347
gerberth92 <gerberthcontreras@gmail.com>
2023-12-19 15:37:10 -0500

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ lista (list): lista donde se guardaran los argumentos pasados.
 """
 
 from sys import argv
-import os.path
+from os.path import exists
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 lista = []
-if os.path.exists("add_item.json"):
+if exists("add_item.json"):
     lista = load_from_json_file("add_item.json")
 
 for i in argv[1:]:
