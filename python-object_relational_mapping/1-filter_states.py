4fb7e74fda35b94e1a196f769c3cacd3bc369d71
gerberth92 <gerberthcontreras@gmail.com>
2024-01-06 00:00:31 -0500

    LIKE

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Este modulo se conecta con un servidor sql.
+"""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """
+    Este es el punto de entrada
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE N%\
+                ORDER BY id asc")
+    states = cur.fetchall()
+
+    for estado in states:
+        print(estado)
+
+if __name__ == "__main__":
+    main()
