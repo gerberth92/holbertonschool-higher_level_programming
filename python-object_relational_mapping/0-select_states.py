11dc32f78d3c1c21a87982c2789c71289c9c363b
gerberth92 <gerberthcontreras@gmail.com>
2024-01-05 22:44:57 -0500

    update 2

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,7 @@ Este modulo se conecta con un servidor sql.
 """
 
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="root",
-                     db="hbtn_0e_0_usa",
-                     port="3306")
+db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="hbtn_0e_0_usa", port="3306")
 
 cur = db.cursor()
 cur.execute("SELECT * FROM states")
