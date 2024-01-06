2b28ab8cc2317c8567af3c67c08e8079c1fa380a
gerberth92 <gerberthcontreras@gmail.com>
2024-01-05 20:33:17 -0800

    cur.fetchall()

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,14 @@ def main():
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
-                         port="3306")
+                         port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
+    states = cur.fetchall()
+
+    for estado in states:
+        print(estado)
 
 if __name__ == "__main__":
     main()
