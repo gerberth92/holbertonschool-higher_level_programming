ae4f34df47363b023ed6344fef4f75de4fd45275
gerberth92 <gerberthcontreras@gmail.com>
2024-01-08 13:05:07 -0500

    %s

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Este modulo se conecta con un servidor sql.
+"""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """
+    Este es el punto de entrada
+
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+        argv[4]: city name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name = %s\
+                ORDER BY id ASC", (argv[4],))
+    states = cur.fetchall()
+
+    for estado in states:
+        print(estado)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
