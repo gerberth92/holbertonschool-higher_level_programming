036bb53189ed833c57358789d7e0fe92dcbe63ec
gerberth92 <gerberthcontreras@gmail.com>
2024-01-14 11:34:50 -0500

    imprimiendo primer registro

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Este modulo se conecta con una base de datos.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+    
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    inicio = sessionmaker(bind=engine)
+    sesion = inicio()
+
+    states = sesion.query(State).order_by(State.id).first()
+
+    if not states:
+        print("Nothing")
+    else:
+        print(f"{states.id}: {states.name}")
+    
+    sesion.close()
+    
