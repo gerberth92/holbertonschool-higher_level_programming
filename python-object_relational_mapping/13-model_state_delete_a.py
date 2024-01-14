664e0d833ee2bccd16511d384c640b6965991c1e
gerberth92 <gerberthcontreras@gmail.com>
2024-01-14 15:30:58 -0500

    .delete()

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Este modulo se conecta con una base de datos.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+    
+if __name__ == "__main__":
+    """
+    Realiza una consulta para buscar un valor en especifico.
+
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    inicio = sessionmaker(bind=engine)
+    sesion = inicio()
+
+    states = sesion.query(State).filter(State.name.like("%a%")).all()
+
+    for state in states:
+        sesion.delete(state)
+
+    sesion.commit()
+    sesion.close()
+    
